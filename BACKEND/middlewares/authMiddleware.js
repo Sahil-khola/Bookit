c48e7a86b039a82a6fe4e6672ab4023e3d84b7f2
Sahil-khola <blackiayaduvanshi@gmail.com>
2026-06-23 22:42:53 +0530

    admin and user Middleware

---
+++ BACKEND/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const protectMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (token) {
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decode.id).select("-password");
+      if (!req.user) {
+        return res.status(401).json({ message: "User unauthorized" });
+      }
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "User unauthorized" });
+    }
+  } else {
+    return res.status(401).json({ message: "User unauthorized" });
+  }
+};
+
+const organizerMiddleware = async (req,res,next) => {
+    if (req.user && req.user.role === "organizer") {
+        next();
+    } else {
+        return res.status(403).json({ message: "Only organizers can access" });
+    }
+}
+
+export {protectMiddleware,organizerMiddleware}
