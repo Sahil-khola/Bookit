2272fad95821e78f9f4b8d30222c173d6f0b5552
Sahil-khola <blackiayaduvanshi@gmail.com>
2026-06-25 10:42:28 +0530

    Add Cluster For Perfomance

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,6 +7,10 @@ import authRoutes from "./routes/authRoutes.js";
 import eventRoutes from "./routes/eventRoute.js";
 import bookingRoutes from "./routes/bookingRoute.js";
 import wishlistRoutes from "./routes/wishlistRoutes.js";
+import http from "http";
+import os from "os";
+import cluster from "cluster";
+const cluser = cluster;
 
 const app = express();
 
@@ -27,13 +31,29 @@ app.use("/api/events",eventRoutes );
 app.use("/api/bookings",bookingRoutes );
 app.use("/api/wishlist",wishlistRoutes );
 
-// Connect to MongoDB
+// Connect to MongoDB----->
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log("Connected to MongoDB"); 
 }).catch((error) => {
     console.error("Error connecting to MongoDB:", error);
 })
 
+
+// Cluster from performance--->
+if (cluser.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    console.log(os.cpus().length);
+    cluser.fork();
+    cluser.fork();
+    cluser.fork();
+}else{
+    http.createServer((req,res)=>{
+        setTimeout(() => {
+        res.end(`hello world ${process.pid}`);
+        }, 2000);
+    }).listen(3000);
+    console.log("http://localhost:3000");
+}
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
