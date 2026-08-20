ae88d64bf2c396444243ac5cf0480b4935b9fbfb
Sahil-khola <blackiayaduvanshi@gmail.com>
2026-08-20 23:51:45 +0530

    bugs

--- BACKEND/controllers/authcontroller.js
+++ BACKEND/controllers/authcontroller.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { sendOTPEmail } from "../utils/email.js";
 import Otp from "../models/OTP.js";
 import jwt from "jsonwebtoken";
--- BACKEND/routes/authRoutes.js
+++ BACKEND/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, logInUser , verifyOtp } from "../controllers/authController.js";
+import { registerUser, logInUser , verifyOtp } from "../controllers/authcontroller.js";
 
 const router = express.Router();
 
